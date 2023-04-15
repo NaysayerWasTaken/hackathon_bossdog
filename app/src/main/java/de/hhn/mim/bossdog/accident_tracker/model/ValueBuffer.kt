7f6c9cf3882fc8cc5ff4d651199374eccd1629d6
pbaracho <pbaracho@stud.hs-heilbronn.de>
2023-04-16 01:21:51 +0200

    R5 is fun

--- app/src/main/java/de/hhn/mim/bossdog/accident_tracker/model/ValueBuffer.kt
+++ app/src/main/java/de/hhn/mim/bossdog/accident_tracker/model/ValueBuffer.kt
@@ -8,15 +8,28 @@ import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
+import org.hl7.fhir.r5.model.CodeableConcept
+import org.hl7.fhir.r5.model.DecimalType
+import org.hl7.fhir.r5.model.Enumerations
+import org.hl7.fhir.r5.model.Observation
+import org.hl7.fhir.r5.model.PrimitiveType
+import org.hl7.fhir.r5.model.Quantity
 
 object ValueBuffer {
     private const val BUFFER_SIZE = 5
 
     @Serializable
-    private val buffer = mutableListOf<DataPoint>()
-
+    private val buffer = mutableListOf<Observation>()
     public suspend fun add(client: HttpClient, time: Instant, value: Double){
-        buffer.add(DataPoint(time, value))
+        val ob = Observation()
+        val con = CodeableConcept()
+        con.addCoding("Wear", "MDC_ECG_ELEC_POTL", "Smartwatch")
+        ob.status = Enumerations.ObservationStatus.FINAL
+        ob.code = con
+        ob.value = Quantity(value)
+
+
+        buffer.add(ob)
         Log.d("Debug", "buffer size: " + buffer.size.toString())
         if (buffer.size == BUFFER_SIZE){
             Log.d("Info", buffer.toString())
